58692b25e8b0743209a3aec3ba9e8159ae3bd665
Lori Kulikowski <soupfairy99@gmail.com>
2018-01-14 12:12:12 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,3 @@
+var chocolateBars = ["snicker","hundred grand","kitkat","skittles"];
+function destructivelyAddElementToBeginningOfArray() {var firstArray = [1]; firstArray.push('foo');}
+function addElementToBeginningOfArray(){var secondArray = [1]; ['foo',...secondArray];}
