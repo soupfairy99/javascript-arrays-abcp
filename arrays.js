8ac7a05f31a3c9b4fb4c4ad377aa54ba6aebf80c
Lori Kulikowski <soupfairy99@gmail.com>
2018-01-19 16:18:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,31 @@
 var chocolateBars = ["snicker","hundred grand","kitkat","skittles"];
-function destructivelyAddElementToBeginningOfArray() {var firstArray = [1]; firstArray.push('foo');return firstArray}
-function addElementToBeginningOfArray(){var secondArray = [1]; secondArrayplus=['foo',...secondArray];return secondArrayplus}
-function destructivelyAddElementToEndOfArray() {var fourthArray = [1]; fourthArray.unshift('foo');return fourthArray}
-function addElementToEndOfArray(){var thirdArray = [1]; thirdArrayplus=[...thirdArray,'foo'];return thirdArrayplus}
-function accessElementInArray(){var sixArray=[1,2,3]; return sixArray[2];}
-function destructivelyRemoveElementFromBeginningOfArray(){const months = [1,2,3]; months.shift; return months;}
-  function removeElementFromBeginningOfArray() {var eightArray = [1,2,3]; eightArray.slice(1); return eightArray}
-function   destructivelyRemoveElementFromEndOfArray(){var nineArray = [1,2,3];eightArray.pop; return eightArray;}
-function removeElementFromEndOfArray(){var tenArray = [1,2,3]; tenArray.slice(0,tenArray.length - 1);return tenArray}
+
+function
+addElementToBeginningOfArray()
+{var array=[1]; var array2 =['foo',...array]; return array2}
+
+function
+destructivelyAddElementToBeginningOfArray() {var array = [1]; return array.unshift("foo")}
+
+/*
+function addElementToBeginningOfArray()
+{
+  var array1=[1]
+  var array2=["foo",...array1]
+  return array2
+}
+addElementToBeginningOfArray()
+
+function destructivelyAddElementToBeginningOfArray()
+{
+  var ArrayDestructAdd =[1]
+  return ArrayDestructAdd.push("foo")
+  function 
+}
+/*
+{
+  var arrayslicelast=[1,2,3]
+  return arrayslicelast.slice(0,array1.length-1)
+  
+}
+*/
