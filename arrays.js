ce1152e51060d8c56e8b402d4b3402e2d2dcf7b6
Lori Kulikowski <soupfairy99@gmail.com>
2018-01-19 16:44:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,24 +13,5 @@ destructivelyAddElementToBeginningOfArray(array,'foo');
 
 
 /*
-function addElementToBeginningOfArray()
-{
-  var array1=[1]
-  var array2=["foo",...array1]
-  return array2
-}
-addElementToBeginningOfArray()
 
-function destructivelyAddElementToBeginningOfArray()
-{
-  var ArrayDestructAdd =[1]
-  return ArrayDestructAdd.push("foo")
-  function 
-}
-/*
-{
-  var arrayslicelast=[1,2,3]
-  return arrayslicelast.slice(0,array1.length-1)
-  
-}
 */
